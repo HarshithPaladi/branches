03fd9964ab0d268bee1da35c213704231f432489
Harshith Paladi <87645227+HarshithPaladi@users.noreply.github.com>
2023-03-21 15:17:25 +0530

    Temp changes

--- 1.py
+++ 1.py
@@ -1,4 +1,8 @@
 print("Hello world")
 print("THis is first file")
+<<<<<<< HEAD
 print("From temp branch")
 print("THis is first file")
+=======
+print("From temp branch")
+>>>>>>> e789a6b (Temp changes)
